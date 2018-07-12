b7886b222bd5de6dabbf2a9551b840443646f6e7
aleschromec <ales.chromec@kiwi.com>
2018-07-12 18:23:43 +0200

    Basic header. Add Prettier.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="PurpleConvert-mainView">
+          <header className="PurpleConvert-header">
+            <div>
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <div>
+              <h1 className="App-title">Welcome to React</h1>
+            </div>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     );
   }
