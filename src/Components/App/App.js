3888a7bd1a92b65e6202bc4c5d8476836414bcad
aleschromec <ales.chromec@kiwi.com>
2018-07-15 08:29:34 +0200

    Refactor App.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import "../../Main.css";
 import Header from "./../Header/Header";
 import Converter from "../Converter/Converter";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Converter />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Converter />
+  </div>
+);
 
 export default App;
