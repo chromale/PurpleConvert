3c9bcf2ddcf378f91d0d3a64d007a6bef2d2751c
aleschromec <ales.chromec@kiwi.com>
2018-07-15 08:27:53 +0200

    Refactor result

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -2,39 +2,24 @@
 import React from "react";
 import "./Result.css";
 
-type State = {
-  to: Object,
-  from: Object,
-  amount: number,
-  result: number
-};
-
 type Props = {
-  result: State
+  result: {
+    to: Object,
+    from: Object,
+    amount: number,
+    result: number
+  }
 };
 
-class Result extends React.Component<Props, State> {
-  state = {
-    ...this.props.result
-  };
-
-  componentWillReceiveProps(nextProps: Props) {
-    this.setState(() => ({ ...nextProps.result }));
-  }
-
-  render() {
-    const { amount, from, to, result } = this.state;
-    return (
-      <div className="Result _shadow">
-        <div className="Result-info">
-          {amount} {from.value} is approx.
-        </div>
-        <div className="Result-main">
-          {result.toFixed(2)} {to.value}
-        </div>
-      </div>
-    );
-  }
-}
+const Result = (props: Props) => (
+  <div className="Result _shadow">
+    <div className="Result-info">
+      {props.result.amount} {props.result.from.value} is approx.
+    </div>
+    <div className="Result-main">
+      {props.result.result.toFixed(2)} {props.result.to.value}
+    </div>
+  </div>
+);
 
 export default Result;
